b537dfa55f18ab7e6f3adb963ae1ea9937921587
jt <jaterli@hotmail.com>
2024-05-24 17:01:13 +0200

    Creación de provider para conectar con Metamask

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
 
 function App() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  console.log("🚀 ~ App ~ provider:", provider);
+
+  const onHandleConnect = async () => {
+    await provider.send("eth_requestAccounts", []);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ethers dApp Meamask demo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onHandleConnect}>Connect with Metamask</button>
       </header>
     </div>
   );
